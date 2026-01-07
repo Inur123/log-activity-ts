d1006ffcec2cb7bbd007ba160dd920d9cd472d86
zainurroziqin <zainurroziqin38@gmail.com>
2026-01-07 09:19:09 +0700

    feat: Add color-scheme meta tag to enhance theme support across layouts

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? config('app.name') }}</title>
-
+    <meta name="color-scheme" content="light">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/logo.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/logo.png') }}">
--- resources/views/components/layouts/super-admin.blade.php
+++ resources/views/components/layouts/super-admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="color-scheme" content="light">
     <title>{{ $title ?? 'Super Admin' }}</title>
 
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo.png') }}">
